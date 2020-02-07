41dae15757ae4ec903a1a3888965b4ac82a8394c
antobrz <56537739+antobrz@users.noreply.github.com>
2020-02-07 15:02:40 +0100

    Add files via upload

---
+++ Kółko i krzyżyk.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void plansza(char tab[i])
+{
+   
+
+   
+}
+
+bool wygranaX()
+{
+    if
+}
+
+int main()
+{
+    
+   plansza(tab);
+
+        system("pause");
+}
